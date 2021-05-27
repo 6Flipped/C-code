ec2563582ec6abe3b86403a3a7808f15ac3c35dc
6Flipped <501536951@qq.com>
2021-05-27 23:08:01 +0800

    字符串指针

---
+++ 5.27.1.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS  1
+#include <stdio.h>
+
+int main()
+{
+	char* string = "I love China!";
+	printf(" % s\n", string);
+	string = "I am a student.";
+	printf(" % s\n", string);
+	return 0;
+}
