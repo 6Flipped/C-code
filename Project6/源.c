746e2ab83d49cd639238572fc0e9bbcfffd2477c
6Flipped <501536951@qq.com>
2021-06-14 23:07:18 +0800

    小

---
+++ Project6/源.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS  1
+#include<stdio.h>
+
+int max(int m, int n)
+{
+	if (m > n)
+		return m;
+	else
+		return n;
+}
+﻿﻿
+; int main()
+{
+	int a, b;
+	scanf("%d%d", &a, &b);
+	a = max(a, b);
+	printf("max=%d", a);
+	return 0;
+}
